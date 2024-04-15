dcfb5c57ebfe50e7f7ed6b4767a0c46f483db5d0
SlowTyper-sys <82166973+SlowTyper-sys@users.noreply.github.com>
2024-04-15 19:07:25 +0200

    optimized the code so that it is easier to read

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+
+const resultElement = document.getElementById('result');
+const playerScoreElement = document.getElementById('playerScore');
+const computerScoreElement = document.getElementById('computerScore');
+
 function Play (playerMove) {
 
     let computerMove = ComputerMove();
@@ -5,34 +10,21 @@ function Play (playerMove) {
         document.getElementById('result').innerHTML = 'Draw';
     }
     else {
-        if((playerMove === 'rock' && computerMove === 'scissor') || (playerMove === 'scissor' && computerMove === 'paper') || (playerMove === 'paper' && computerMove === 'rock')) {
-            document.getElementById('result').innerHTML = 'You Win!';
-            document.getElementById('playerScore').innerHTML = parseInt(document.getElementById('playerScore').innerHTML) + 1;
-    } else{
-        document.getElementById('result').innerHTML = 'You Lose :(';
-        document.getElementById('computerScore').innerHTML = parseInt(document.getElementById('computerScore').innerHTML) + 1;
+        if((playerMove === 'rock' && computerMove === 'scissor') || 
+            (playerMove === 'scissor' && computerMove === 'paper') || 
+            (playerMove === 'paper' && computerMove === 'rock')) {
+            resultElement.innerHTML = 'You Win!';
+            playerScoreElement.innerHTML = parseInt(playerScoreElement.innerHTML) + 1;
+        } else{
+            document.getElementById('result').innerHTML = 'You Lose :(';
+            computerScoreElement.innerHTML = parseInt(computerScoreElement.innerHTML) + 1;
+        }
     }
-}
-
 
 }
 
 function ComputerMove () {
-    let computerChoice = '';
+    let list = ['rock', 'paper', 'scissor'];
     let compChoice = Math.floor(Math.random() * 3) + 1;
-    switch (compChoice) {
-        case 1:
-            computerChoice = 'rock';
-            break;
-        case 2:
-            computerChoice = 'paper';
-            break;
-        case 3:
-            computerChoice = 'scissor';
-            break;
-    
-        default:
-            break;
-    }
-    return computerChoice;
+    return list[compChoice -1];
 }
